c35f3aa2abd8aa19ed223646fc8928a97975da58
Ray <mindcultivateinfo@gmail.com>
2026-05-18 22:58:17 +0200

    CORS frontendOrigin setup

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,17 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:5173'
-}));
+const frontendOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+  process.env.FRONTEND_URL
+].filter(Boolean);
+
+app.use(
+  cors({
+    origin: frontendOrigins,
+  })
+);
 
 app.use(express.json());
 
